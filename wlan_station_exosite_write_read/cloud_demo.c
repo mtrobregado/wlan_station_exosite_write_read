245231c4c9684699d44bf1011b0f387a30d7171b
mtrobregado <mtrobregado@yahoo.com.ph>
2015-12-19 23:51:10 +0800

    Update cloud_demo.c

--- wlan_station_exosite_write_read/cloud_demo.c
+++ wlan_station_exosite_write_read/cloud_demo.c
@@ -1,14 +1,15 @@
 //*****************************************************************************
 //
-// Maker/Author - Markel T. Robregado
+// Write and Read data to Exosite Cloud using Tiva Connected Launchpad + CC3100 BP + Sensor Hub BP,
+// Connects to AP through WiFi
 //
+// Maker/Author - Markel T. Robregado *
 // Modification Details : Write and read data to Exosite.
-//                        The Program sends temperature data from Sensor Hub, switch press count status
-//                        to Exosite using TI SimpleLink CC3100. It also reads Exosite portal dashboard,
-//                        if the dasboard switches has been pressed. If dashboard switch has been pressed
-//                        a Tiva Connected Launchpad on-board led will be turned on. Two Exosite portal
-//                        dashboards are available to turn on two on-board leds. *
-// Device Setup: Tiva Connected Launchpad + CC3100 Booster pack + Sensor Hub Booster Pack
+// Sends Temperature data from TMP006 sensor to Exosite Cloud
+// Sends switch press count to Exosite Cloud
+// on-board led on-off, from Exosite Dashboard Switches
+//
+// Device Setup: Tiva Connected Launchpad + CC3100 Booster pack + Sensor Hub Booster Pack * */
 //
 //*****************************************************************************
 
